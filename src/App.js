95e2d2889b058f9613440fae800fa4ed18017438
RADRI <radridri@gmail.com>
2024-02-12 15:36:49 +0000

    Add listing card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import listingData from "./server/data.json";
 import Header from "./components/header.js";
+import ListingCard from "./components/listingCard.js";
 
 function App() {
+  const listings = listingData.properties;
+
   return (
     <div classname="temp">
       <Header />
+      <ListingCard rentPerMonth={listings[0]['rent per month']} img={listings[0].pic} address={listings[0].address} id={listings[0].id} bed={listings[0]['property-type'].bed} bath={listings[0]['property-type'].bath} />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
