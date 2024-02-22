35ea6dd15036002cd5123a29da92e8318378ac2e
yingqi949 <fany@tcd.ie>
2024-02-22 17:57:01 +0800

    back to homepage btn

--- src/pages/Listing/index.js
+++ src/pages/Listing/index.js
@@ -1,12 +1,17 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import { Button } from 'antd'
 
 const Listing = () => {
+    const navigate = useNavigate()
     const params = useParams()
     let id = params.id
 
     return (
         <div>
             this is property {id}
+            <Button type="primary" onClick={()=>{navigate('/')}}>
+                Back to Homepage
+            </Button>
         </div>
     )
 
