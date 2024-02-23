e2a0bba61648896a0ae2d261d1c295b7e1050485
RADRI <radridri@gmail.com>
2024-02-23 13:10:38 +0000

    Fix card css

--- src/components/listingCard.js
+++ src/components/listingCard.js
@@ -7,7 +7,7 @@ export default function ListingCard(props) {
         <section className="Card">
             <img src={props.img[0]} className="Card-Image" alt="Listing"/>
             <a clasname="Address" href={link}>{props.address}</a>
-            <h1 clasname="Rent">Rent - {props.rentPerMonth}€</h1>
+            <b clasname="Rent">Rent - {props.rentPerMonth}€</b>
             <h2 clasname="Info">Beds {props.bed} Baths {props.bath}</h2>
         </section>
     );
--- src/components/listingGrid.js
+++ src/components/listingGrid.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import ListingCard from "./listingCard.js";
 import './listingGrid.css';
 
