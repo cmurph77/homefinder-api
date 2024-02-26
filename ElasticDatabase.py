2e82250d91b01048d0ffa323b771c65a60a45e37
Cian O'Gorman <29048772+CianOG123@users.noreply.github.com>
2024-02-26 22:55:45 +0000

    Added property search functions

--- ElasticDatabase.py
+++ ElasticDatabase.py
@@ -14,34 +14,55 @@ class ElasticDatabase:
     def info(self):
         return self.elasticsearch.info()
 
-    def search(self, index, query):
-        try:
-            result = self.es.search(index=index, body=query)
-            hits = result['hits']['hits']
-            return hits
-        except Exception as e:
-            print(f"An error occurred during search: {e}")
-            return ['Error']
+    def search(self, numberOfResults, pageNumber):
+        startIndex = (pageNumber - 1) * numberOfResults
+        query = {
+            "size": numberOfResults,
+            "from": startIndex,
+            "query": {
+                "match_all": {}
+            }
+        }
+        searchResult = self.elasticsearch.search(index="property-listings", body=query)
+        properties = []
+        all_hits = searchResult['hits']['hits']
+        for num, doc in enumerate(all_hits):
+            for key, value in doc.items():
+                if(key == "_source"):
+                    properties.append(value)
+        return properties
+
+
+
+    def searchWithField(self, numberOfResults, pageNumber, field):
+        startIndex = (pageNumber - 1) * numberOfResults
+        query = {
+            "size": numberOfResults,
+            "from": startIndex,
+            "sort": field,
+            "query": {
+                "match_all": {}
+            }
+        }
+        searchResult = self.elasticsearch.search(index="property-listings", body=query)
+        properties = []
+        all_hits = searchResult['hits']['hits']
+        for num, doc in enumerate(all_hits):
+            for key, value in doc.items():
+                if(key == "_source"):
+                    properties.append(value)
+        return properties
         
 
 # Test connection
 def main():
-        
     client = ElasticDatabase()
-    json = client.info()
-    print(client.info())
-
-if __name__ == "__main__":
-    main()
-
-def main():
-    print('ready...')
-
-    client = ElasticDatabase()
-    print(client.info())
-
-    print('All done :)')
-
+    numberOfResults = 50
+    pageNumber = 1
+    sortBy = 'address'
+    properties = client.searchWithField(numberOfResults, pageNumber, sortBy)
+    for prop in properties:
+        print(f"{prop['identifier']}, {prop['address']}, {prop['rent per month']}")
 
 if __name__ == "__main__":
     main()
@@ -50,23 +71,16 @@ if __name__ == "__main__":
 # ---------------------------------------------------------------------------------------------------------
 # Example:
 
-# Create instance call it whatever you want
+# Create instance, name it whatever you want
+# client = ElasticDatabase()
 
-# elastic_db = ElasticDatabase()
+# Return the 50 properties for page 1
+# properties = client.search(50, 1)
 
+# Return the next 50 properties for page 2
+# properties = client.search(50, 2)
 
-
-# perform a search like this :)
-# This depends on the structure of the json (better functions coming soon!!!)
-
-# index = 'your_index_name'
-# query = {
-#     "query": {
-#         "match": {
-#             "title": "example"
-#         }
-#     }
-# }
-# result = elastic_db.search(index, query)
+# Return the 50 properties for page 4 sorted by rent (low to high)
+# properties = client.searchWithField(50, 4, 'rent per month')
 
 # ---------------------------------------------------------------------------------------------------------
