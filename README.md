7b52d2b1eff3e55a2d4e0b42968e9f455c5eaa5b
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-02-27 15:42:51 +0000

    configured to call the API updaed the readme.md to contain instructions on use

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+# Instructions to run this code
+1. First you must clone the backend code for this project at - it is the api-main of the homefinder repository
+    link:  https://github.com/cmurph77/homefinder/tree/api-main
+2. Follow the instructions in the README.md file of api-main to launch the containers for database and backend code
+3. You can verify if the  backend is set up by going to 'http://localhost:8000/' . There should be a welome message from the homefinder server
+4. Now in this directory, run 'npm install' to install all node dependencies
+5. Finaly run 'npm start' to start up the front end in development mode
+6. You wil be presented with the login screen - You can create your own account or use the test account details belw
+    email: test@test.com
+    password: 123456
 
-In the project directory, you can run:
 
 ### `npm start`
 
@@ -13,4 +19,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,40 @@ import { Button, message } from 'antd'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/firebase'
 import { useNavigate } from 'react-router-dom'
-import listingData from "@/server/daftData.json";
+// import listingData from "@/server/daftData.json";
 import Header from "@/components/header.js";
 import ListingGrid from "@/components/listingGrid.js";
 
-const listings = listingData.properties;
+// const old_listings = listingData.properties
+// console.log("Frontend Property Data - oldlistings ")
+// console.log(old_listings)
+
+// Make the API call --------------------------------------
+
+var listings
+await fetch('http://127.0.0.1:8000/dummydata-properties')
+  .then(response => {
+    // Check if the request was successful (status code 200)
+    if (!response.ok) {
+      throw new Error('Failed to retrieve data');
+    }
+    // If successful, parse the JSON data
+    return response.json();
+  })
+  .then(api_data => {
+    // Now you can work with the JSON data
+    listings = api_data.properties
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+console.log("listings recieved from the backend")
+console.log(listings)
+
+
 const Home = () => {
     const navigate = useNavigate()
     const signout = () => {
