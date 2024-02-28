fa7f359f71f25af7429648169abb98d58d9fa4f8
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-02-28 16:00:18 +0000

    added in sample endpoint for pagesize/pagenum

--- app.py
+++ app.py
@@ -29,38 +29,39 @@ def get_properties():
         # Return an error message if the file does not exist
         return {'error': 'properties.json file not found'}, 404
 
-# Endpoint to get property by ID
-@app.route('/get-property-by-id/<int:property_id>', methods=['GET'])
-def get_property(property_id):
-    property_data = ElasticDatabase.searchByID(property_id)
-    if property_data:
-        return property_data
+
+# # Endpoint to get property by ID
+# @app.route('/get-property-by-id/<int:property_id>', methods=['GET'])
+# def get_property(property_id):
+#     current_time = datetime.now()
+#     print("GET REQ - /get-property-by-id  id:"+ str(property_id)+"   @ [" + str(current_time) + "]")
+#     property_data = ElasticDatabase.searchByPropertyID(property_id)
+#     if property_data:
+#         return property_data
+#     else:
+#         return {'error': 'Property not found'}, 404
+
+
+@app.route('/get-propertys-by-pagenum-live/<int:pagenum>/<int:numresults>', methods=['GET'])
+def get_propertys_pagesize(pagenum,numresults):
+    current_time = datetime.now()
+    print("GET REQ - /get-propertys-by-pagenum  pagenum: " +str(pagenum) + ", pagesize:" +str(numresults)+" @ [" + str(current_time) + "]")
+    data = ElasticDatabase.search(numresults,pagenum)
+    return data
+
+@app.route('/get-propertys-by-pagenum-sample/<int:pagenum>/<int:numresults>', methods=['GET'])
+def get_propertys_pagesize_sample(pagenum,numresults):
+    current_time = datetime.now()
+    print("GET REQ - /get-propertys-by-pagenum  pagenum: " +str(pagenum) + ", pagesize:" +str(numresults)+" @ [" + str(current_time) + "]")
+
+    fieldSearchPath = os.path.join(os.getcwd(), 'mock_data','ExampleJSONs', 'FieldSearch50ByRentExample.json')
+
+    # Check if the file exists
+    if os.path.exists(fieldSearchPath):
+        # Return the JSON file
+        return send_file(fieldSearchPath, mimetype='application/json')
     else:
-        return {'error': 'Property not found'}, 404
-
-
-@app.route('/get-property-by-id/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    print(user_id)
-    return {
-            "id": "  ",
-            "address": "Samle Property",
-            "rent per month": 0,
-            "daft.ie link": "  ",
-            "latitude": 53.341833017884795,
-            "longitude": -6.288615427057721,
-            "property-type": {
-                "category": "  ",
-                "type": [
-                    "  ",
-                    "  ",
-                    "  "
-                ],
-                "bed": "  ",
-                "bath": "  ",
-                "m2": "N/A"
-            }
-        }
-
+        # Return an error message if the file does not exist
+        return {'error': 'json file not found'}, 404
 
 
---
+++ sample_script.py
@@ -0,0 +1,4 @@
+from ElasticDatabase import ElasticDatabase
+
+data = ElasticDatabase.search(50,2)
+print(data)
