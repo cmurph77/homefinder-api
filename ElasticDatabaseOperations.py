bae686c75b507cc58927285f1cdf3bd7851a4101
Cian O'Gorman <29048772+CianOG123@users.noreply.github.com>
2024-03-17 15:25:06 +0000

    Added Users database
    The database for users has been added.
    
    Running database operations will now produce three elastic indexes:
    • Property Listings
    • Property Likes
    • Users

--- ElasticDatabaseOperations.py
+++ ElasticDatabaseOperations.py
@@ -9,8 +9,9 @@ from ElasticDatabase import ElasticDatabase
 class ElasticDatabaseOperations:
     def __init__(self):
         self.client = ElasticDatabase()
-        self.propertyListingsMapping = ElasticDatabaseOperations.createPropertyListingsIndexMapping()
+        self.propertyListingsMapping = ElasticDatabaseOperations.createPropertyListingsMapping()
         self.propertyLikesMapping = ElasticDatabaseOperations.createPropertyLikesMapping()
+        self.usersMapping = ElasticDatabaseOperations.createUsersMapping()
 
     def populateDatabaseWithPropertyListingsData(self):
         indexName = 'property-listings'
@@ -39,7 +40,7 @@ class ElasticDatabaseOperations:
             self.client.elasticsearch.indices.delete(index=indexName)
 
         print(f'Creating index \"{indexName}\"')
-        self.client.elasticsearch.indices.create(index=indexName, body=self.propertyListingsMapping)
+        self.client.elasticsearch.indices.create(index=indexName, body=self.propertyLikesMapping)
 
         propertyIdJson = os.path.join('mock_data', 'webscraping', 'property_ids.json')
         print(f'Reading json \"{propertyIdJson}\"')
@@ -54,6 +55,25 @@ class ElasticDatabaseOperations:
         self.indexPropertyLikesData(propertyIdData['property_ids'], mockUserData, indexName)
 
         print(f'\"{mockUserJson}\" indexing complete')
+
+    def populateDatabaseWithUsersData(self):
+        indexName = 'users-db'
+
+        if self.client.elasticsearch.indices.exists(index=indexName):
+            print(f'Deleting index \"{indexName}\"')
+            self.client.elasticsearch.indices.delete(index=indexName)
+
+        print(f'Creating index \"{indexName}\"')
+        self.client.elasticsearch.indices.create(index=indexName, body=self.usersMapping)
+
+        jsonName = os.path.join('mock_data', 'mockUsers', 'mock_users.json')
+        print(f'Reading json \"{jsonName}\"')
+        data = self.readJSON(jsonName)
+
+        print('Indexing (please wait for confirmation)...')
+        self.indexUsersData(data, indexName)
+
+        print(f'\"{jsonName}\" indexing complete')
 
     def indexPropertyListingsData(self, data, indexName):
         for jsonData in data[indexName]:
@@ -95,6 +115,28 @@ class ElasticDatabaseOperations:
 
             self.client.elasticsearch.index(index=indexName, id=propertyId, body=doc)
 
+    def indexUsersData(self, data, indexName):
+        for jsonData in data:
+            doc = {
+                'firebase_id' : jsonData['firebase_id'],
+                'name' : jsonData['name'],
+                'profile_pic' : jsonData['profile_pic'],
+                'selected_tags':{
+                    'languages' : jsonData['selected_tags']['language'],
+                    'smoker' : jsonData['selected_tags']['smoker'],
+                    'pets' : jsonData['selected_tags']['pets'],
+                    'diet' : jsonData['selected_tags']['diet'],
+                    'allergies' : jsonData['selected_tags']['allergies'],
+                    'habit' : jsonData['selected_tags']['habit'],
+                    'work' : jsonData['selected_tags']['work']
+                },
+                'phone_number' : jsonData['phone_number'],
+                'bio' : jsonData['bio'],
+                'liked_properties' : jsonData['liked_properties'],
+                'liked_users' : jsonData['liked_users'],
+            }
+            self.client.elasticsearch.index(index=indexName, id=jsonData['firebase_id'], document=doc)
+
     @staticmethod
     def readJSON(fileName):
         with open(fileName) as file:
@@ -102,7 +144,7 @@ class ElasticDatabaseOperations:
         return data
 
     @staticmethod
-    def createPropertyListingsIndexMapping():
+    def createPropertyListingsMapping():
         mapping = {
             "mappings": {
                 "properties": {
@@ -165,12 +207,82 @@ class ElasticDatabaseOperations:
             }
         }
         return propertyLikesMapping
+    
+    @staticmethod
+    def createUsersMapping():
+        mapping = {
+            "mappings": {
+                "properties": {
+                    "firebase_id":{
+                        "type": "keyword"
+                    },
+                    "name":{
+                        "type": "text"
+                    },
+                    "profile_pic":{
+                        "type": "keyword"
+                    },
+                    "selected_tags":{
+                        "type": "nested", "properties": {
+                            "languages": {
+                                "type": "keyword",
+                                "fields": {
+                                    "text": { "type": "keyword" }
+                                }
+                            },
+                            "smoker": {
+                                "type": "keyword"
+                            },
+                            "pets": {
+                                "type": "keyword"
+                            },
+                            "diet": {
+                                "type": "keyword"
+                            },
+                            "allergies": {
+                                "type": "keyword"
+                            },
+                            "habit": {
+                                "type": "keyword"
+                            },
+                            "work": {
+                                "type": "keyword"
+                            }
+                        }
+                    },
+                    "phone_number":{
+                        "type": "keyword"
+                    },
+                    "bio":{
+                        "type": "text"
+                    },
+                    "liked_properties":{
+                        "type": "keyword",
+                        "fields": {
+                            "text": { "type": "keyword" }
+                        }
+                    },
+                    "liked_users":{
+                        "type": "keyword",
+                        "fields": {
+                            "text": { "type": "keyword" }
+                        }
+                    }
+                }
+            }
+        }
+        return mapping
+
 
 
 def main():
     elasticDatabaseOperations = ElasticDatabaseOperations()
+    print('\n')
     elasticDatabaseOperations.populateDatabaseWithPropertyListingsData()
+    print('\n')
     elasticDatabaseOperations.populateDatabaseWithPropertyLikesData()
+    print('\n')
+    elasticDatabaseOperations.populateDatabaseWithUsersData()
     print('All done!')
     
 if __name__ == "__main__":
