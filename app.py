b1d1b1ba6665c7db81e246082795fae64cc9da91
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-03-27 20:14:58 +0000

    put endpoints in different sections in file

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)  # Enable CORS for all routes
 def hello_world():
     return 'Hello, Welcome to the homefinder Server!'
 
-# ---------------- CONNNECTED TO DATABASE -------------------------------------
+# ---------------- CONNNECTED TO DATABASE + OPERATIONAL -------------------------------------
 # This returns a property object of the given 'id'
 @app.route('/get-property-by-id-live/<int:property_id>', methods=['GET'])
 def get_property(property_id):
@@ -25,6 +25,7 @@ def get_property(property_id):
     else:
         return {'error': 'Property not found'}, 404
 
+
 # This returns a list of propertys that should be ont he specified pagenum
 @app.route('/get-propertys-by-pagenum-live/<int:pagenum>/<int:numresults>', methods=['GET'])
 def get_propertys_pagesize(pagenum,numresults):
@@ -32,6 +33,7 @@ def get_propertys_pagesize(pagenum,numresults):
     print("GET REQ - /get-propertys-by-pagenum  pagenum: " +str(pagenum) + ", pagesize:" +str(numresults)+" @ [" + str(current_time) + "]")
     data = ElasticDatabase.search(numresults,pagenum)
     return data
+
 
 # Tries to return liked property values if found
 @app.route('/get-liked-properties/<string:user_id>', methods=['GET'])
@@ -51,42 +53,7 @@ def get_liked_properties(user_id):
     else:
         return {'error': 'User liked properties not found'}, 404
 
-# this endpoint takes a user_id and updates user info
-# NEEDS VERIFICATION
-@app.route('/update-users-info', methods=['POST'])
-def update_user_info(user_id):
-    request_data = request.get_json()
-    user_id = request_data['user_id']
 
-    # DEBUG 
-    #user_id = "YSixicUz"
-    #root_dir = os.path.dirname(os.path.abspath(__file__))
-    #file_path = os.path.join(root_dir, 'sample.JSON')
-    #with open(file_path, 'r')as file:
-    #    user_data = json.load(file)
-
-    # logic to update the user data in database
-    ElasticDatabase.updateDatabaseUser(userID=user_id, userData=request_data)
-
-    return { 'message': 'successfully updated'}, 200
-
-# Takes json data from frontend accompanied with request in same format as database index send json in body
-# NEEDS VERIFICATION !
-@app.route('/add-user-to-database/', methods=['POST'])
-def create_user():
-    print('-- RECEIEVED REQUEST TO ADD NEW USER\n')
-    try :
-        user_data = request.get_json()
-        print(user_data)
-
-    except: 
-        return {'error': 'Could not read request user data'}, 404
-    try:
-        ElasticDatabase.addNewUserToDatabase(user_data)
-        return "Success", 200
-    except:
-        return {'error': 'Could not add user to database'}, 404
-    
 # this endpoint takes a user_id and retruns all the data associated with the user
 @app.route('/get-user-info/<string:user_id>', methods=['GET'])
 def get_user_info(user_id):
@@ -96,6 +63,7 @@ def get_user_info(user_id):
         return {'error': 'user info not found'}, 404
     else:
         return user_info , 200
+
 
 @app.route('/like-property/<string:user_id>/<int:property_id>',methods=['PUT'])
 def like_property(user_id,property_id):
@@ -111,6 +79,7 @@ def like_property(user_id,property_id):
     except:
         return {'error': 'failed to like property'}, 404
     
+    
 @app.route('/unlike-property/<string:user_id>/<int:property_id>',methods = ['PUT'])
 def unlike_property(user_id,property_id):
     try:
@@ -124,7 +93,51 @@ def unlike_property(user_id,property_id):
     except:
         return {'error': 'failed to unlike property'}, 404
 
-# -------------------------- under development -------------------------------------
+
+# -------------------------- NOT VERIFIED - STILL TESTING  -------------------------------------
+# this endpoint takes a user_id and updates user info
+# NEEDS VERIFICATION
+@app.route('/update-users-info', methods=['POST'])
+def update_user_info(user_id):
+    try:
+        request_data = request.get_json()
+    except: return { 'error' : 'could not rease request data'}
+    try:
+        user_id = request_data['firebase_id']
+    except: return { 'error' : 'could not rease user_id from data'}
+    # DEBUG 
+    #user_id = "YSixicUz"
+    #root_dir = os.path.dirname(os.path.abspath(__file__))
+    #file_path = os.path.join(root_dir, 'sample.JSON')
+    #with open(file_path, 'r')as file:
+    #    user_data = json.load(file)
+
+    # logic to update the user data in database
+    try:
+        ElasticDatabase.updateDatabaseUser(userID=user_id, userData=request_data)
+    except: return {'error' : 'error adding to database'}
+
+    return { 'message': 'successfully updated'}, 200
+
+# Takes json data from frontend accompanied with request in same format as database index send json in body
+# NEEDS VERIFICATION !
+@app.route('/add-user-to-database/', methods=['POST'])
+def create_user():
+    print('-- RECEIEVED REQUEST TO ADD NEW USER\n')
+    try :
+        user_data = request.get_json()
+        print(user_data)
+    except: 
+        return {'error': 'Could not read request user data'}, 404
+    try:
+        ElasticDatabase.addNewUserToDatabase(user_data)
+        return "Success", 200
+    except:
+        return {'error': 'Could not add user to database'}, 404
+    # =================================================================================================
+
+
+# -------------------------- UNDER DEVELOPMENT -------------------------------------
 app.route('/update-users-liked-properties', methods=['POST'])
 def update_users_liked_properties():
     # Extract JSON data from the request
