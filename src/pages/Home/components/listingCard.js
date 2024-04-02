843a9af3d0b4ddfc5124bdc1583d5487f869611c
RADRI <radridri@gmail.com>
2024-04-02 13:45:34 +0100

    Fix card css

--- src/pages/Home/components/listingCard.js
+++ src/pages/Home/components/listingCard.js
@@ -8,7 +8,7 @@ export default function ListingCard(props) {
             <img src={props.img} className="Card-Image" alt="Listing"/>
             <a className="Address" href={link}>{props.address}</a>
             <b className="Rent">Rent - {props.rentPerMonth}€</b>
-            <h2 className="Info">Beds {props.bed} Baths {props.bath}</h2>
+            <h2 className="Info">{props.bed} {props.bath}</h2>
         </section>
     );
 }
