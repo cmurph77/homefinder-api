3bb584ff21268ce6aa1b53e4d893521ae32b0f3e
yingqi949 <fany@tcd.ie>
2024-04-04 00:34:28 +0800

    User Details

--- src/pages/Listing/components/UserList.js
+++ src/pages/Listing/components/UserList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { Avatar, Divider, List, Skeleton } from 'antd';
-import { axios_instance } from '@/utils'
+import { Avatar, Divider, List, Skeleton, Modal, Descriptions } from 'antd';
+import { axios_instance } from '@/utils';
 
 const UserList = () => {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [currentUser, setCurrentUser] = useState(null);
+
     const loadMoreData = () => {
         if (loading) {
             return;
@@ -27,47 +30,71 @@ const UserList = () => {
         loadMoreData();
     }, []);
 
+    const showModal = (user) => {
+        setCurrentUser(user);
+        setModalVisible(true);
+    };
+
+    const handleCancel = () => {
+        setModalVisible(false);
+    };
+
     return (
-        <div
-            id="scrollableDiv"
-            style={{
-                height: 400,
-                overflow: 'auto',
-                padding: '0 16px',
-                border: '1px solid rgba(140, 140, 140, 0.35)',
-            }}
-        >
-        <InfiniteScroll
-            dataLength={data.length}
-            next={loadMoreData}
-            hasMore={data.length < 50}
-            loader= {
-                <Skeleton
-                    avatar
-                    paragraph={{
-                        rows: 1,
-                    }}
-                    active
-                />
-            }
-            endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-            scrollableTarget="scrollableDiv"
-        >
-            <List
-            dataSource={data}
-            renderItem={(item) => (
-                <List.Item key={item.email}>
-                <List.Item.Meta
-                    avatar={<Avatar src={item.picture.large} />}
-                    title={<a href="https://ant.design">{item.name.last}</a>}
-                    description={item.email}
-                />
-                <div>Content</div>
-                </List.Item>
-            )}
-            />
-        </InfiniteScroll>
-        </div>
+        <>
+            <Modal
+                title="User Detail"
+                open={modalVisible}
+                onCancel={handleCancel}
+                footer={null}
+            >
+                <Descriptions bordered column={1}>
+                    <Descriptions.Item label="Avatar" span={1} rowspan={2} ><Avatar src={currentUser?.picture?.large} /></Descriptions.Item>  
+                    <Descriptions.Item label="Name">{currentUser?.name?.first} {currentUser?.name?.last}</Descriptions.Item>
+                    <Descriptions.Item label="Gender">{currentUser?.gender}</Descriptions.Item>
+                    <Descriptions.Item label="Phone Number">{currentUser?.email}</Descriptions.Item>
+                    <Descriptions.Item label="Nationality">{currentUser?.nat}</Descriptions.Item>
+                </Descriptions>
+            </Modal>
+            <div
+                id="scrollableDiv"
+                style={{
+                    height: 400,
+                    overflow: 'auto',
+                    padding: '0 16px',
+                    border: '1px solid rgba(140, 140, 140, 0.35)',
+                }}
+            >
+                <InfiniteScroll
+                    dataLength={data.length}
+                    next={loadMoreData}
+                    hasMore={data.length < 50}
+                    loader={
+                        <Skeleton
+                            avatar
+                            paragraph={{
+                                rows: 1,
+                            }}
+                            active
+                        />
+                    }
+                    endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                    scrollableTarget="scrollableDiv"
+                >
+                    <List
+                        dataSource={data}
+                        renderItem={(item) => (
+                            <List.Item key={item.email}>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={item.picture.large} />}
+                                    title={<a onClick={() => showModal(item)}>{item.name.last}</a>}
+                                    description={item.email}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </InfiniteScroll>
+            </div>
+        </>
     );
 }
 
